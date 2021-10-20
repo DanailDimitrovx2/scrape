93b4e9801955da0856649d0e16407dc92743b98b
Danail Dimitrov <danail.dimitrov@plana.solutions>
2021-10-21 01:49:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+
+
+def scrape_website(url):
+        driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+        driver.get(url)
+        dict = {}
+        dict['name'] = driver.find_element(By.XPATH, '//*[@id="app"]/main/div/div[3]/div[1]/div[1]/h1').text
+        dict['price'] = float(driver.find_element(By.XPATH, '//*[@id="app"]/main/div/div[3]/div[1]/div[2]/meta[2]').get_attribute('content'))
+        dict['color'] = driver.find_element(By.XPATH, '//*[@id="app"]/main/div/div[3]/div[2]/div[2]/span').text
+        sizes = driver.find_elements(By.CLASS_NAME, 'size-unavailable')
+        sizes.extend(driver.find_elements(By.CLASS_NAME, 'size-available'))
+        dict['size'] = [x.get_attribute('data-size') for x in sizes]
+
+        return json.dumps(dict)
+
+
+url = 'https://shop.mango.com/gb/women/skirts-midi/midi-satin-skirt_17042020.html?c=99'
+json = scrape_website(url)
+a=2
